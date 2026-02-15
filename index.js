fe4a4b0cb0c4c356508fb496ae15a720d4bc7d2c
arpegx <arpegx@posteo.de>
2026-02-16 00:10:01 +0100

    update headliner

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, Portainer!');
+  res.send('<h1>Hello, Portainer!</h1>');
 });
 
 app.listen(port, () => {
